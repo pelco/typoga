b2037c50f514855cf95194ea8509872954cd6090
pelco <pelco89@gmail.com>
2018-07-01 01:36:52 +0100

    This will close #5 added categories to game.

--- lib/functions.sh
+++ lib/functions.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Function that initializes misFile at the beginning of each phrase
 function init_miss_f {
     echo "" >> $misFile
@@ -28,4 +30,20 @@ function checkScoresFile {
     if [ ! -f $misFile ]; then
         cp -f scores/misses.txt $misFile
     fi
+}
+
+declare -A category
+for i in {0..4}; do category[$i]="Snail"; done
+for i in {5..9}; do category[$i]="Walker"; done
+for i in {10..14}; do category[$i]="Jogger"; done
+for i in {15..19}; do category[$i]="Bicycle"; done
+for i in {20..29}; do category[$i]="Dog"; done
+for i in {30..44}; do category[$i]="Bolt"; done
+for i in {45..49}; do category[$i]="Fox"; done
+for i in {50..64}; do category[$i]="Tiger"; done
+for i in {65..69}; do category[$i]="Gazelle"; done
+for i in {70..79}; do category[$i]="Horse"; done
+for i in {80..120}; do category[$i]="CHETAH"; done
+function getCategory {
+    echo "${green}${category[$1]}${reset} ($1)"
 }
