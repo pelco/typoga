dd1be72397c83b068ae08893f5e63b56430dc786
pelco <pelco89@gmail.com>
2019-02-11 15:07:07 +0000

    commented code

--- scores.py
+++ scores.py
@@ -11,12 +11,13 @@ RANDOM = "Random"
 PH_LEADERS = "Leaders"
 PROGRAMMING = "Programming"
 
-gameOptions = [
+GameOptions = [
     RANDOM,
     PH_LEADERS,
     PROGRAMMING ]
 
-socreOptions = [
+# The order must match exactly how scores are saved in the highscore file
+SocreOptions = [
     "Scores",
     "Accuracy",
     "Words per minute",
@@ -55,63 +56,58 @@ class TypogaDraw:
 
     def checkBoxesInit(self):
         """
+        Initialize check boxes and click events.
         """
         # Check which score files exist.
         self.checkForScoreFiles()
 
         # Initialize CheckBox Menu for Game Options
         raxGO = plt.axes([0.02, 0.5, 1, 0.5], frameon = False)
-        self.checkButGO = CheckButtons(raxGO, gameOptions, (False, False, False))
+        self.checkButGO = CheckButtons(raxGO, GameOptions, (False, False, False))
         # Set CheckBox handler for game Options
         self.checkButGO.on_clicked(self.handleUserSelection)
 
         # Initialize CheckBox Menu for Score Options
         raxSO = plt.axes([0.02, 0.05, 1, 0.5], frameon = False)
-        self.checkButSO = CheckButtons(raxSO, socreOptions,
+        self.checkButSO = CheckButtons(raxSO, SocreOptions,
                     (False, False, False, False, False, False, False, False))
         # Set CheckBox handler for game Options
         self.checkButSO.on_clicked(self.handleUserSelection)
 
     def handleUserSelection(self, label):
         """
+        This method is called every time a check box gets pressed. 
         """
-
-        # Divide plot based on user selection
-        rows = self.checkButSO.get_status().count(True)
-
-        # Draw Random selected data
-        fig = plt.figure(num=gameOptions[0], figsize=(6,8))
+        # Draw Random data in its figure
+        fig = plt.figure(num=GameOptions[0], figsize=(6,8))
         if self.checkButGO.get_status()[0] == True :
-            self.pos = 1
             fig.clear()
-            self.parseFile(self.randFilePath, rows)
+            self.parseFile(self.randFilePath)
             fig.show()
         else:
             plt.close()
 
-        # Draw Leaders selected data
-        fig = plt.figure(num=gameOptions[1], figsize=(6,8))
+        # Draw Leaders data in its figure
+        fig = plt.figure(num=GameOptions[1], figsize=(6,8))
         if self.checkButGO.get_status()[1] == True :
-            self.pos = 1
             fig.clear()
-            self.parseFile(self.leadFilePath, rows)
+            self.parseFile(self.leadFilePath)
             fig.show()
         else:
             plt.close()
 
-        # Draw Programming selected data
-        fig = plt.figure(num=gameOptions[2], figsize=(6,8))
+        # Draw Programming data in its figure
+        fig = plt.figure(num=GameOptions[2], figsize=(6,8))
         if self.checkButGO.get_status()[2] == True :
-            self.pos = 1
             fig.clear()
-            self.parseFile(self.progFilePath, rows)
+            self.parseFile(self.progFilePath)
             fig.show()
         else:
             plt.close()
 
-    def parseFile(self, pfile, rows):
+    def parseFile(self, pfile):
         """
-        Method used to parse pfile and draw grafics
+        Method used to parse pfile and plot graphics
         """
         # Check if file exists
         if not os.path.exists(pfile):
@@ -123,19 +119,23 @@ class TypogaDraw:
             lines = hsf.readlines()
             lines = lines[3:] # skip header
 
+            # Reset plot Position
+            self.pos = 1
             for i in range(len(self.checkButSO.get_status())):
-                scores = []
+                scores = [] # Clear previous scores
                 if self.checkButSO.get_status()[i] == True:
+                    # Parse each score line
                     for scoreLine in lines:
                         scoreLine = scoreLine.strip() # remove \n
                         scoreLine = scoreLine.split(":")
 
                         scores.append(float(scoreLine[i+1])) # store data
+                    # plot scores data in 
+                    self.plotData(scores, i)
 
-                    self.plotData(scores, rows, i)
-
-    def plotData(self, scores, rows, index):
+    def plotData(self, scores, index):
         """
+        Plot score data
         """
         if len(scores) == 0:
             scores.append(0)
@@ -146,21 +146,26 @@ class TypogaDraw:
         scores.append(0)
 
         x = np.arange(1, len(scores)+1, 1)
-        
+
+        # Divide plot based on user selection
+        rows = self.checkButSO.get_status().count(True)
         plt.subplot(rows, 1, self.pos)
         self.pos += 1
 
-        plt.ylabel(socreOptions[index])
+        plt.ylabel(SocreOptions[index])
         plt.xticks(x)
+
         # Draw bar plot
         plt.bar(x, scores)
         # Add text legends to each bar
         for i in range(len(scores)-1): # -1 because we appended zero at the end
             plt.text(i+0.8, scores[i]/2, ("%.02f" % scores[i]), fontsize=8, color='black')
+
         # Draw orange bar for the highest value
         max_val = np.argmax(scores)
         plt.bar(max_val+1, scores[max_val], color='orange')
-        #plt.text(max_val+1, scores[max_val], ("%.02f" % scores[max_val]), fontsize=10, color='black')
+        # TODO remove this line:
+        # plt.text(max_val+1, scores[max_val], ("%.02f" % scores[max_val]), fontsize=10, color='black')
 
         # Draw mean line
         plt.plot([0, len(scores)], [mean, mean], 'black')
